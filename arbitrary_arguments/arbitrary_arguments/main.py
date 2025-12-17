a07fa5eec89625c1374e8e023ad9bd7982b62a7b
FarvaNet <>
2025-12-17 19:00:10 +0000

    Arbitrary Arguments

---
+++ arbitrary_arguments/arbitrary_arguments/main.py
@@ -0,0 +1,18 @@
+def calculate_total(___):
+    if ___:
+        return "Your cart is empty."
+
+    total = ___
+
+    if ___
+        total *= ___  # 20% discount
+    elif ___:
+        total *= ___  # 10% discount
+
+    return f"Final total: ${total:.2f}"
+
+# Testing the result
+print(calculate_total(30, 20, 50))
+print(calculate_total(100, 50, 80))
+print(calculate_total(150, 100))
+print(calculate_total())
